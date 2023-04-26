1cb2d3213cd5a0cb9dd6b0581e15d01539dbca66
Yi Fan Li <yifanli123@gmail.com>
2023-04-26 15:05:56 -0400

    changed action space to discrete

--- Drone2D_ENV_forRL_with_Uncertainty_package/Drone_tester.py
+++ Drone2D_ENV_forRL_with_Uncertainty_package/Drone_tester.py
@@ -1,11 +1,11 @@
 import Drone2D_ENV_forRL_with_Uncertainty_env
 import gymnasium as gym
 
-newenv = gym.make("Drone2D-Uncertain-v0", render_sim = True, Sensor_noise_level = "none",
-                   Actuator_noise_level = "none", Environmental_disturbance = "none")
+newenv = gym.make("Drone2D-Uncertain-v0", render_sim = True, Sensor_noise_level = "medium",
+                   Actuator_noise_level = "none", Environmental_disturbance = "constant")
 
 obs, info = newenv.reset()
 
-for i in range (100):
-    obs, reward, terminated, truncated, info = newenv.step([500,800])
+for i in range (10):
+    obs, reward, terminated, truncated, info = newenv.step([0,0])
 print(obs)
